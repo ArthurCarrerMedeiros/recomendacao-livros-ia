7a80d31eca2538bed8b099db15e5f0be1e9194d4
Arthur Carrer Medeiros <100293067+ArthurCarrerMedeiros@users.noreply.github.com>
2025-03-20 19:48:05 -0300

    feat: desativando validação do CORS

--- api-be/main.py
+++ api-be/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers.recomendacao_livros import router
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
