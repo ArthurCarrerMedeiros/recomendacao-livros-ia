566f15a4edd7c79042e1d53f9795a5cefead650c
Arthur Carrer Medeiros <100293067+ArthurCarrerMedeiros@users.noreply.github.com>
2025-04-01 19:12:26 -0300

    feat: criando classe para conexão com Gemini AI

--- api-be/app/clients/genai_client.py
+++ api-be/app/clients/genai_client.py
@@ -1,14 +1,21 @@
 from google import genai
+from app.models.livro import Livro
 
 class GenAIClient:
     def __init__(self, api_key: str):
         self.client = genai.Client(api_key=api_key)
 
     def gerar_resposta(self, mensagem: str):
+        condicao = "\nMe retorne esses dados no formato JSON"
         response = self.client.models.generate_content(
-            model="gemini-2.0-flash", contents=mensagem
+            model="gemini-2.0-flash", contents=mensagem + condicao,
+            config={
+                'response_mime_type': 'application/json',
+                'response_schema': list[Livro],
+            },
         )
-        if response:
-            return response.text
+        if(response):
+            livros_recomendados: list[Livro] = response.parsed
+            print(livros_recomendados)
         else:
             raise Exception('Erro ao enviar mensagem para gemini ai')
